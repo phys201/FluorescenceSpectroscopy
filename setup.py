b99bceea95bde259969a68cefbdc05dc0f38b74b
Brodi <bdelwood@fas.harvard.edu>
2022-04-12 22:25:08 -0400

    Initial pass at setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='example',
-      version='1.0',
-      description='An example package for Harvard PHYS201',
-      url='http://github.com/phys201/example',
-      author='ralex0',
-      author_email='ralex0@users.noreply.github.com',
+      version='0.1',
+      description='1',
+      long_description=readme(),
+      url='http://github.com/phys201/FluorescenceSpectroscopy',
+      author='bdelwood, derickgonzalez, abdullahnasir-97',
+      author_email='bdelwood@users.noreply.github.com',
       license='GPLv3',
-      packages=['example'],
+      packages=['fluospec'],
       install_requires=['numpy'])
