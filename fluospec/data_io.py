dddf14fb12fd62f00a594a5ef9cca1ae986c9e14
Brodi Elwood <bdelwood@fas.harvard.edu>
2022-04-20 01:23:41 -0400

    add todo to data_io

--- fluospec/data_io.py
+++ fluospec/data_io.py
@@ -10,6 +10,8 @@ import pandas as pd
 from pathlib import Path
         
 
+# TODO: metadata when we have a better idea what final data will look like
+
 def load_data(data_file: Union[str, Path]
     ) -> pd.DataFrame:
     return pd.read_csv(data_file)
