3df957440b0e5a70a112ece9c3e48a599f4c6744
Brodi Elwood <bdelwood@fas.harvard.edu>
2022-04-27 00:31:29 -0400

    fixing tests for model.py

--- fluospec/tests/test_model.py
+++ fluospec/tests/test_model.py
@@ -1,10 +1,8 @@
 import unittest
 from unittest import TestCase
 
-from pathlib import Path
-
 from fluospec.model import FluoSpecModel
-from fluospec.data_io import load_data
+from fluospec.data_io import load_data, get_data_file_path
 from pymc3 import Model
 
 model_sim_params = {
@@ -16,9 +14,11 @@ model_sim_params = {
                     'b_prior_params': (2, .04),
                     }
 
+sim_data_path = get_data_file_path('fluospec_sim_data.pkl')
+
 class TestModel(TestCase):
     def test_FluoSpecModel_returns_Model(self):
-        data_df = load_data(Path('../sim_data/fluospec_sim_data.csv'))
+        data_df = load_data(sim_data_path)
         fluospec_model_instance = FluoSpecModel(**model_sim_params)
         generative_model = fluospec_model_instance.model(data_df)
         self.assertTrue(isinstance(generative_model, Model))
