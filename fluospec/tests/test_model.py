195f1067ab2be4e982392a366f18f7e04e28a0de
Brodi Elwood <bdelwood@fas.harvard.edu>
2022-05-03 22:14:50 -0400

    fix tests and add new test for new class

--- fluospec/tests/test_model.py
+++ fluospec/tests/test_model.py
@@ -3,19 +3,33 @@ from unittest import TestCase
 
 from pathlib import Path
 
-from fluospec.model import Prediction
-from fluospec.model import FluoSpecModel
-from  fluospec.model import SimulateFluoSpec
+from fluospec.model import Prediction, FluoSpecModel, SimulateFluoSpec, SpectralLines
 from fluospec.data_io import load_data, get_data_file_path
 from pymc3 import Model
 from os.path import exists
 
-theta = {'A': 2,
-         'w0': 20,
-         'gamma': 5,
-         'intensity_ratio': .5,
+theta = {'spectral_lines': [
+                           SpectralLines(2,
+                                         30,
+                                         5,
+                                         .5
+                                        ),
+                            SpectralLines(1,
+                                          10,
+                                          4,
+                                          .5
+                                         )
+                           ],
          'm': .05,
-         'b': 2}
+         'b': 2,
+         }
+
+class TestSpectralLines(TestCase):
+    spec_line = theta['spectral_lines'][0]
+    def test_SpectralLines(self):
+        keys = list(vars(self.spec_line).keys())
+        self.assertEqual(keys,
+                         ['A', 'w0', 'gamma', 'intensity_ratio'])
 
 class TestModel(TestCase):
     sim_data_path = get_data_file_path('fluospec_sim_data.pkl')
@@ -25,23 +39,30 @@ class TestModel(TestCase):
 
     def test_lorentzian(self):
         pred = Prediction.init_with_defaults()
-        w_res = pred.w0
-        pred.intensity_ratio = 0
-        loren = pred.lorentzian(w_res)
-        self.assertEqual(loren, pred.A)
+        spec_line = pred.spectral_lines[0]
+        w_res = spec_line.w0
+        spec_line.intensity_ratio = 0
+        loren = spec_line.lorentzian(w_res)
+        self.assertEqual(loren, spec_line.A)
 
     def test_prediction_line(self):
         w = 1
         pred = Prediction.init_with_defaults()
-        pred.A = 0
+        spec_line_0 = pred.spectral_lines[0]
+        spec_line_1 = pred.spectral_lines[1]
+        spec_line_0.A = 0
+        spec_line_1.A = 0
         line = pred.m*w + pred.b
         self.assertEqual(pred.prediction(w), line)
 
     def test_prediction_lorentzian(self):
         pred = Prediction.init_with_defaults()
+        spec_line_0 = pred.spectral_lines[0]
+        spec_line_1 = pred.spectral_lines[1]
         pred.m = 0
         pred.b = 0
-        self.assertEqual(pred.prediction(1), pred.lorentzian(1))
+        self.assertEqual(pred.prediction(1),
+                         spec_line_0.lorentzian(1) + spec_line_1.lorentzian(1))
 
     def test_generate_sim_data(self):
         sim = SimulateFluoSpec.init_with_defaults()
@@ -62,13 +83,13 @@ class TestModel(TestCase):
 
     def test_FluoSpecModel_returns_Model(self):
         model_sim_params = {
-                    'A_prior_params': (2, 1),
-                    'w0_prior_params': (20, 4),
-                    'gamma_prior_params': (5, 1),
-                    'intensity_ratio_prior_params': (.5, .1),
-                    'm_prior_params': (.05, .01),
-                    'b_prior_params': (2, .04),
-                    }
+                            'line_prior_params': [
+                                                ((2, 1), (30, 2), (5, 1), (.5, .1)),
+                                                ((1, 1), (10, 2), (4, .5), (.5, .1))
+                                                 ],
+                            'm_prior_params': (.0, .01),
+                            'b_prior_params': (2, .04),
+                            }
         data_df = load_data(self.sim_data_path)
         fluospec_model_instance = FluoSpecModel(**model_sim_params)
         generative_model = fluospec_model_instance.model(data_df)
